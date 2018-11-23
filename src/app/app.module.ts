3a6fd6b5473e873a9c73538d2f8e4a8f7f2c9906
wsmr <w.s.m.rathnasekara@gmail.com>
2018-11-23 17:05:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import {InAppBrowser} from '@ionic-native/in-app-browser';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -22,6 +24,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   providers: [
+    InAppBrowser,
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { OnInit } from '@angular/core';
+
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private iab: InAppBrowser) {
 
   }
-
+  ngOnInit(){
+    const browser = this.iab.create('http://www.mgconsultants.lk/','_self',{location:'no'});
+  }
 }
